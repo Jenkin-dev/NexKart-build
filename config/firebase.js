fb50716536e2ef785d76702dc0516639919ece32
C_Jenkins <gbengachris87@gmail.com>
2026-02-21 17:40:17 +0100

    `Added Firebase dependencies and updated package.json and package-lock.json`

---
+++ config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKiTswxyifHdkQbwUQdmfbsC_ogNhIpvY",
+  authDomain: "nexkart-29780.firebaseapp.com",
+  projectId: "nexkart-29780",
+  storageBucket: "nexkart-29780.firebasestorage.app",
+  messagingSenderId: "144679913401",
+  appId: "1:144679913401:android:1b8aeeb7c0f39e88789868",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
